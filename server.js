f974189075e0a7052560aa9f8476b9ca63ea2546
sandond77 <sandu@ucdavis.edu>
2018-03-16 01:16:08 -0700

    end of first attempt

--- server.js
+++ server.js
@@ -4,7 +4,15 @@ var mongoose = require("mongoose");
 var cheerio = require("cheerio");
 var request = require("request");
 
-var PORT = 3000;
+var port = process.env.PORT || 3000;
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI, {
+  useMongoClient: true
+});
 
 // Require all models
 var db = require("./models");
@@ -15,14 +23,13 @@ var app = express();
 // Serve static content for the app from the 'public' directory
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
-
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({ 
 	defaultLayout: 'main',
-	layoutsDir: path.join(__dirname, 'views/layouts')
+	layoutsDir: path.join(__dirname, 'views')
  }));
 app.set('view engine', 'handlebars');
 
@@ -31,9 +38,6 @@ app.set('view engine', 'handlebars');
 var routes = require('./controllers/controller.js');
 app.use(routes);
 
-var port = process.env.PORT || 3000;
-db.sequelize.sync().then(function(){  //take force true out before heroku commit
-	app.listen(port, function(){
-		console.log("App now listening at localhost:" + port);
-	});
-})
+app.listen(port, function(){
+	console.log("App now listening at localhost:" + port);
+});
